b464a647d1b4ba9d6dd44a4fc11745bd42b7c2f6
Rogers Ruga <140224981+rogersruga@users.noreply.github.com>
2025-05-06 14:06:23 +0300

    Assignment correction answers.sql

--- answers.sql
+++ answers.sql
@@ -1,14 +1,18 @@
 -- Question 1 Achieving 1NF (First Normal Form)
 
-SELECT 
-    pd.OrderID,
-    pd.CustomerName,
-    TRIM(jt.Product) AS Product
-FROM ProductDetail pd,
-JSON_TABLE(
-    CONCAT('[', REPLACE(pd.Products, ',', '","'), ']'),
-    '$[*]' COLUMNS(Product VARCHAR(100) PATH '$')
-) AS jt;
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
+);
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
 
 
 
@@ -20,24 +24,21 @@ CREATE TABLE Orders (
     OrderID INT PRIMARY KEY,
     CustomerName VARCHAR(100)
 );
-
 INSERT INTO Orders (OrderID, CustomerName)
 VALUES
 (101, 'John Doe'),
 (102, 'Jane Smith'),
 (103, 'Emily Clark');
 
--- Table 2: OrderItems (Only product and quantity per order)
 
-CREATE TABLE OrderItems (
+CREATE TABLE Product (
     OrderID INT,
     Product VARCHAR(100),
     Quantity INT,
     PRIMARY KEY (OrderID, Product),
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
-
-INSERT INTO OrderItems (OrderID, Product, Quantity)
+INSERT INTO Product (OrderID, Product, Quantity)
 VALUES
 (101, 'Laptop', 2),
 (101, 'Mouse', 1),
